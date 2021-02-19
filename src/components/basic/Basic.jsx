4763bfe34f6105235ffe0d558a9bca3cfadb9460
chris k <yovirinc@gmail.com>
2021-02-19 14:02:07 +0100

    making data

--- src/components/basic/Basic.jsx
+++ src/components/basic/Basic.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import TopDownloadBar from '../TopDownloadBar'
 
+import { basicDataA } from '../data/basic'
+
 const Basic = () => {
   return (
     <div>
       <TopDownloadBar enlace='https://drive.google.com/file/d/1J5AQrSrptI0w3D9gjOqdqooNFEKGWvUZ/view?usp=sharing' />
+      <PrepositionTag />
+      <div className='max-width-container container-padding'>
+        <h2>A</h2>
+        {
+          basicDataA.map(data => {
+            return (<div>
+              <h4>{data.title}</h4>
+              <p>{data.means}</p>
+              {data.example.map(ex => {
+                return(<div>
+                  <p>{ex.ex}</p>
+                  <p>{ex.exMean}</p>
+                </div>)
+              })}
+              <p>{data.example.ex}</p>
+              <p>{data.example.exMean}</p>
+              <p>{data.tip ? data.tip.split('\n').map(str => <p>{str}</p>) : ''}</p>
+            </div>)
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+const PrepositionTag = () => {
+  return(
+    <div>
+      prep tags
     </div>
   )
 }
---
+++ src/components/data/basic.js
@@ -0,0 +1,218 @@
+export const basicDataA = [
+  {
+    title: 'a + algo (사물)',
+    means: '추가 또는 뺄 요소',
+    example: [
+      {
+        ex: 'Échale sal a la sopa.',
+        exMean: '수프에 소금을 넣어라.'
+      },{
+        ex: 'He añadido unos párrafos al informe.',
+        exMean: '보고서에 문장 몇 개를 더 추가했어.'
+      }
+    ]
+  },
+  {
+    title: 'a + alguien (사람)',
+    means: '사람 앞에',
+    example: [
+      {
+        ex: 'El otro día vi a Juan en el supermercado.',
+        exMean: '저번에 마트에서 후안을 봤어.'
+      },{
+        ex: 'Llama a María, que venga inmediatamente.',
+        exMean: '마리아를 불러. 지금 바로 오게.'
+      }
+    ],
+    tip: `* 목적어가 아닌 일반적으로 말할 땐 전치사 a를 쓰지 않는다. (~ 몇 명 또는 직업 등)
+    ex) Busco una secretaria que hable tres idiomas.
+    3개국어를 할 수 있는 비서를 찾고 있어.`
+  },
+  {
+    title: 'a + alguien / algo (사람 또는 사물)',
+    means: '어떤 행동을 최종적으로 받는 사람 또는 사물',
+    example: [
+      {
+        ex: 'Le he comprado este libro a Miguel.',
+        exMean: '나는 이 책을 미겔에게 사 주었다.'
+      },{
+        ex: 'A mí no me gusta nada el fútbol. y a tí?',
+        exMean: '난 축구가 정말 별로야. 너는?'
+      }
+    ],
+    tip: `* para + persona
+    a + 사람/사물과 같은 뜻을 의미하지만 a보다 수신인을 더 강조한다.
+    ser 동사 뒤에 수신인을 가리키고 싶을 때는 항상 para가 나온다. (es a tí (X))`
+  },
+  {
+    title: 'a + fase de desarrollo (상황의 진행)',
+    means: '상황의 진행',
+    example: [
+      {
+        ex: 'Al pricipio de estar aquí no conocía a nadie, pero ahora sí.',
+        exMean: '처음엔 아는 사람이 아무도 없었지만 지금은 있어.'
+      },{
+        ex: 'El tema era dificil de entender, pero al final de la clase, todo parecía claro.',
+        exMean: '이해하기 어려운 주제였지만 수업 막바지에는 모든게 명확해 보였다.'
+      }
+    ]
+  },
+  {
+    title: '¡a + infinitivo / sustantivo! (부정사 / 명사)',
+    means: '명령',
+    example: [
+      {
+        ex: '¡a comer!',
+        exMean: '먹으러 가! 먹어!'
+      },{
+        ex: '¡venga, al colegio!',
+        exMean: '자, 학교 가야지!'
+      }
+    ]
+  },
+  {
+    title: 'a + lugar (장소)',
+    means: '도착점을 가리킴',
+    example: [
+      {
+        ex: 'Mañana llegaré a la oficina tarde.',
+        exMean: '내일 사무실에 좀 늦게 도착할거야.'
+      },{
+        ex: 'Vienes al cine esta noche?',
+        exMean: '오늘밤 영화관에 올거니?'
+      }
+    ],
+    tip: `* hacia + lugar 방향을 가리킴
+    Estoy yendo hacia la biblioteca.
+    난 도서관 방향으로 가고 있다.
+    
+    * hasta + lugar 이동의 도착지점을 가리킴 
+    Este tren no va hasta Barcelona.
+    이 열차는 바르셀로나까지 가지 않습니다.
+    
+    * para + lugar 도달할 수 있거나 또는 그렇지 않은 목표지점이나 사물을 가리킴
+    My voy para el supermercado. vienes conmigo?
+    난 마트에 갈거야. 같이 갈래?
+    
+    * por + lugar ~을 통해서 가는 방법, 수단
+    El ladrón entró por la ventana.
+    도둑은 창문을 통해서 들어왔다.`
+  },
+  {
+    title: 'a + lugar no concreto (구체적이지 않은 장소)',
+    means: '왼쪽으로, 오른쪽으로..',
+    example: [
+      {
+        ex: `Dónde hay una farmacia?
+        - sí, mira, gira la segunda calle a la derecha, allí hay una farmacia.`,
+        exMean: `약국이 어디에 있나요?
+        - 네, 두번째 골목에서 오른쪽으로 꺾으시면 거기 약국이 하나 있어요. `
+      }
+    ]
+  },
+  {
+    title: 'a + modo (방법)',
+    means: '어떤 일을 하기 위한 방법',
+    example: [{
+      ex: 'Tienes que cocer la carne a fuego lento.',
+      exMean: '그 고기는 약한 불로 쪄야 돼.'
+    }],
+    tip: `* en + modo
+    방법 그러나 형태가 있는, 행동, 태도, 스타일.
+    a + modo는 물리적, 기계적인, 생산적인 방법을 의미함.
+    Lo dijo en voz alta, a gritos.
+    그는 큰 목소리로, 소리쳤다.`
+  },
+  {
+    title: 'a + numero + por + unidad (a + 숫자 + por + 단위)',
+    means: '속도나 리듬을 의미',
+    example: [{
+      ex: 'Este coche corre a 200 kilómetros por hora.',
+      exMean: '이 차는 시속 200km로 달립니다.'
+    }, {
+      ex: 'Hacemos paquetes a 30 productos por día.',
+      exMean: '우리는 하루에 30개의 제품을 만듭니다.'
+    }]
+  },
+  {
+    title: 'a + numero de medida de tiempo (걸리는 시간)',
+    means: '시간적 거리',
+    example: [{
+      ex: `Tardaremos mucho en llegar?
+      - la casa de tu abuela está a cuatro horas de nuestra casa.`,
+      exMean: `도착까지 많이 걸려요?
+      - 너희 할머니댁은 우리집에서 4시간 걸려.`
+    }, {
+      ex: 'Mi casa está a dos minutos de aquí.',
+      exMean: '우리집은 여기서 2분거리입니다.'
+    }]
+  },
+  {
+    title: 'a + precio (가격)',
+    means: '변할 수 있는 가격',
+    example: [{
+      ex: `Cuál es el cambio del euro hoy?
+      - el euro está a un dólar con veinte centimos.`,
+      exMean: `오늘 유로 환율이 어떻게 되요?
+      - 1유로에 1달러 20센트입니다.`
+    }, {
+      ex: 'Un kilo de manzanas está a 2 euros.',
+      exMean: '사과 1킬로에 2유로이다.'
+    }],
+    tip: `* en + precio
+    얼마 정도 하겠다는 어림가격 
+    Este coche tan viejo lo puedes vender en 600 euros, no creo que más.
+    이 낡은 차는 600 유로에 팔 수 있을거야, 더 이상은 못받을 것 같아.
+    * por + precio 
+    협상(흥정)을 통해 지불한 가격
+    Lo he comprado por sólo 20 euros, es bastante barato, no?
+    그걸 단돈 20유로에 샀어. 꽤 싸지 않아?`
+  },
+  {
+    title: 'a + el/la + tiempo (시간)',
+    means: '어떤 일을 주기적으로 하는 빈도',
+    example: [{
+      ex: 'Tienes que tomar 3 pastillas al día.',
+      exMean: '너는 하루에 3알씩 약을 먹어야 해.'
+    }, {
+      ex: 'Voy al gimnasio 5 veces a la semana. (por semana)',
+      exMean: '나는 일주일에 5번씩 헬스장(체육관)에 간다.'
+    }],
+    tip: `(= por + tiempo 같은 뜻)`
+  },
+  {
+    title: 'a + el/la ... siguiente',
+    means: '이전에 언급된 상황과 관련된 사건이 일어난 시간',
+    example: [{
+      ex: 'Estuve con ella en el cine. al día siguiente salimos fuera juntos otra vez.',
+      exMean: '난 그녀와 영화관에 있었다. 다음날 우리는 또 같이 밖으로 나갔다.'
+    }],
+    tip: `* al cabo de 
+    과거의 연속된 두 사건에 대해 말할 때
+    Se conocieron en octubre y al cabo de seis meses se casaron.
+    둘은 10월에 만났고 6개월 후 결혼했다.`
+  },
+  {
+    title: 'a la(s)',
+    means: '시간 앞에',
+    example: [{
+      ex: 'Normalmente en España, la hora de comer es a las dos.',
+      exMean: '스페인에서는 보통 2시가 점심시간이다.'
+    }, {
+      ex: 'Empieza la fiesta a las nueve por la tarde.',
+      exMean: '파티는 오후 9시에 시작한다.'
+    }]
+  },
+  {
+    title: 'a la(s)',
+    means: '시간 앞에',
+    example: [{
+      ex: 'Se puso a bailar a lo loco.',
+      exMean: '그는 미친 듯이 춤을 추었다.'
+    }, {
+      ex: 'Tenía tanta hambre que comió a lo bestia.',
+      exMean: '그는 너무 배가 고팠기에 짐승처럼 먹었다.'
+    }]
+  }
+
+] // ends array A
